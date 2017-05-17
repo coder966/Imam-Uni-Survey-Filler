4470bba4a065afaec8f465891d57d15827b3922e
Khalid <coder966@gmail.com>
2017-05-17 15:23:35 +0300

    Add submit button

--- main.js
+++ main.js
@@ -5,4 +5,12 @@ window.onload = function() {
 			code: 'var nodes = document.getElementsByName("SINGLE_RSP"); for(var i=0; i<nodes.length; i++){nodes[i].value = '+options.value+';}'
 		});
 	});
+
+	// handle submitting
+	var submit = document.getElementById("submit");
+	submit.addEventListener("click", function(){
+		chrome.tabs.executeScript({
+			code: 'document.getElementsByName("submit_btn")[1].click();'
+		});
+	})
 }
